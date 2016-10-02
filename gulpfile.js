0810d8369cd38723055b30a73e6709f07e9cea3e
maliknur <malik.dba.pro@gmail.com>
2016-10-01 23:36:08 -0700

    setup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+
 
 // run server
-gulp.task('start', function() {
+gulp.task('server', function() {
 	gulp.src('app')
 		.pipe(server({
 			livereload: true,
-			directoryListing: true,
+			port: 7700,
 			open: true
 		}));
-})
+});
+
+// compile sass
+gulp.task('style', function() {
+	return gulp.src('app/sass/**/*.sass')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(prefix({
+			browsers: ['last 15 versions']
+		}))
+		.pipe(gulp.dest('app/css'));
+});
+
+// watch
+gulp.task('watch', function() {
+	gulp.watch('app/sass/**/*.sass', ['style']);
+});
+
+// default task
+gulp.task('default', ['server', 'watch']);
